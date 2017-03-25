25361a94f41ae38b0c1200052111a47d5f12e494
esp32de <ESP32DE@users.noreply.github.com>
2017-03-25 17:45:18 +0100

    add Component Can Driver as menuconfig compatible
    2017-03-25 rudi ;-) ( http://esp32.de )
    this branch contains the CAN Driver as third party component
    now you can set CAN Driver by menuconfig.
    
    copy this component to your ESP-IDF
    under the folder components and you have this CAN Driver PACK
    allways in your future projects in menuconfig.
    
    if you ony want this component in your project,
    simple copy the component folder with CAN Driver Pack
    to the project folder.
    
    yourProject
    yourProject -> components
    yourProject -> components -> can
    ..
    yourProject -> main
    ..

--- README.md
+++
@@ -1,7 +0,0 @@
-# ESP32-CAN-Driver
-CAN driver project for the Espressif ESP32
-
-Download and install the ESP IDF framework from https://github.com/espressif/esp-idf. The CAN project works the same way as the other example projects.
-
-More Infos here:
-http://www.barth-dev.de/can-driver-esp32/
--- components/can/component.mk
+++ components/can/component.mk
@@ -7,5 +7,10 @@
 # please read the ESP-IDF documents if you need to do this.
 #
 
-COMPONENT_ADD_INCLUDEDIRS 	:= / drv/CAN/include
-COMPONENT_SRCDIRS			:= / drv/CAN drv/CAN/cfg
+# COMPONENT_ADD_INCLUDEDIRS 	:= / drv/CAN/include
+# COMPONENT_SRCDIRS			:= / drv/CAN drv/CAN/cfg
+
+# neu - zumindest dann leer
+
+# COMPONENT_ADD_INCLUDEDIRS 	:= / 
+# COMPONENT_SRCDIRS			:= / 
